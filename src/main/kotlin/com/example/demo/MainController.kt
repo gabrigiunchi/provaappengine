168146316870ab958f548f14df43dd590db9756d
Gabriele Giunchi <gabriele.giunchi1994@gmail.com>
2020-05-07 12:49:47 +0200

    version 0.0.2

--- src/main/kotlin/com/example/demo/MainController.kt
+++ src/main/kotlin/com/example/demo/MainController.kt
@@ -1,16 +1,21 @@
 package com.example.demo
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/alive")
+@RequestMapping("/")
 class MainController {
+
+
+    @Value("\${application.version}")
+    private lateinit var version: String
 
     @GetMapping
     fun foo(): ResponseEntity<String> {
-        return ResponseEntity.ok("Hello!")
+        return ResponseEntity.ok(this.version)
     }
 }
